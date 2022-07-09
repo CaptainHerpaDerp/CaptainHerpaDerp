85bb42bf0bd768d8444f87fc94253b0b8a63c8c1
Ruben Smith <49171379+CaptainHerpaDerp@users.noreply.github.com>
2022-07-09 11:50:56 +0200

    Update and rename README.md to README.cs

---
+++ README.cs
@@ -0,0 +1,11 @@
+  public class Ruben
+  {
+    string name = "Ruben Smith";
+
+    string pronouns = "He | Him";
+
+    string[] languages = {"Python", "Java", "C#", "HTML", "CSS"};
+
+    string studyingAt = "Saxion University of Applied Sciences";
+    
+    }
