c1e13c7b49e09a8aa8c752af8800ed918abf599a
Ruben Smith <49171379+CaptainHerpaDerp@users.noreply.github.com>
2022-07-09 12:01:28 +0200

    Update README.cs

--- README.cs
+++ README.cs
@@ -1,12 +1,139 @@
-  public class Ruben
-  {
-  
-    string name = "Ruben Smith";
+<h2><img src="https://emojis.slackmojis.com/emojis/images/1531849430/4246/blob-sunglasses.gif?1531849430" width="30"/> नमस्ते (Namaste)🙏🏻, I'm Anmol Pratap Singh! <img src="https://media.giphy.com/media/12oufCB0MyZ1Go/giphy.gif" width="50"></h2>
+<img align='right' src="https://media.giphy.com/media/M9gbBd9nbDrOTu1Mqx/giphy.gif" width="230">
+<p><em>Software Engineer at <a href="https://www.footloose.io/">Footloose Labs
+</a><img src="https://media.giphy.com/media/WUlplcMpOCEmTGBtBW/giphy.gif" width="30"> 
+</em></p>
 
-    string pronouns = "He | Him";
+[![Twitter Follow](https://img.shields.io/twitter/follow/misteranmol?label=Follow)](https://twitter.com/intent/follow?screen_name=misteranmol)
+[![Linkedin: anmol](https://img.shields.io/badge/-anmol-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/anmol-p-singh/)](https://www.linkedin.com/in/anmol098/)
+![GitHub followers](https://img.shields.io/github/followers/anmol098?label=Follow&style=social)
+[![website](https://img.shields.io/badge/Website-46a2f1.svg?&style=flat-square&logo=Google-Chrome&logoColor=white&link=https://anmolsingh.me/)](https://anmolsingh.me/)
+![](https://visitor-badge.glitch.me/badge?page_id=anmol098.anmol098)
+![Waka Readme](https://github.com/anmol098/anmol098/workflows/Waka%20Readme/badge.svg)
 
-    string[] languages = {"Python", "Java", "C#", "HTML", "CSS"};
+### 📫 Like to meet me?
 
-    string studyingAt = "Saxion University of Applied Sciences";
-    
-}
+Pick a slot if you'd like to meet me and chat about anything you are passionate about - but make sure to describe the agenda
+
+<a href="https://calendly.com/anmol098/30min" target="_blank"><img width="498" alt="meet_link" src="https://user-images.githubusercontent.com/15426564/144297439-f530f383-e73e-41e0-9914-a9b7d3f432e5.png"></a>
+
+👇 Hit in your console or terminal to connect with me.
+
+```bash
+npx anmol
+```
+**👆 This command line tool can be found at [npx anmol](https://github.com/anmol098/npx_card)**
+
+### <img src="https://media.giphy.com/media/VgCDAzcKvsR6OM0uWg/giphy.gif" width="50"> A little more about me...  
+
+```javascript
+const anmol = {
+    pronouns: "He" | "Him",
+    code: ["Javascript", "Python", "Java", "PHP"],
+    askMeAbout: ["web dev", "tech", "app dev", "photography"],
+    technologies: {
+        backEnd: {
+            js: ["Node", "Fastify", "Express"],
+        },
+        mobileApp: {
+            native: ["Android Development"]
+        },
+        devOps: ["AWS", "Docker🐳", "Route53", "Nginx"],
+        databases: ["mongo", "MySql", "sqlite"],
+        misc: ["Firebase", "Socket.IO", "selenium", "open-cv", "php", "SuiteApp"]
+    },
+    architecture: ["Serverless Architecture", "Progressive web applications", "Single page applications"],
+    currentFocus: "Building Robots to ease opertations",
+    funFact: "There are two ways to write error-free programs; only the third one works"
+};
+```
+
+<img src="https://media.giphy.com/media/LnQjpWaON8nhr21vNW/giphy.gif" width="60"> <em><b>I love connecting with different people</b> so if you want to say <b>hi, I'll be happy to meet you more!</b> 😊</em>
+
+---
+<!--START_SECTION:waka-->
+![Code Time](http://img.shields.io/badge/Code%20Time-1%2C749%20hrs%2017%20mins-blue)
+
+![Profile Views](http://img.shields.io/badge/Profile%20Views-1335-blue)
+
+![Lines of code](https://img.shields.io/badge/From%20Hello%20World%20I%27ve%20Written-1%20Million%20lines%20of%20code-blue)
+
+**🐱 My GitHub Data** 
+
+> 🏆 531 Contributions in the Year 2022
+ > 
+> 📦 169.1 kB Used in GitHub's Storage 
+ > 
+> 🚫 Not Opted to Hire
+ > 
+> 📜 17 Public Repositories 
+ > 
+> 🔑 21 Private Repositories  
+ > 
+**I'm an Early 🐤** 
+
+```text
+🌞 Morning    66 commits     ██░░░░░░░░░░░░░░░░░░░░░░░   9.55% 
+🌆 Daytime    285 commits    ██████████░░░░░░░░░░░░░░░   41.24% 
+🌃 Evening    226 commits    ████████░░░░░░░░░░░░░░░░░   32.71% 
+🌙 Night      114 commits    ████░░░░░░░░░░░░░░░░░░░░░   16.5%
+
+```
+📅 **I'm Most Productive on Sunday** 
+
+```text
+Monday       93 commits     ███░░░░░░░░░░░░░░░░░░░░░░   13.46% 
+Tuesday      75 commits     ██░░░░░░░░░░░░░░░░░░░░░░░   10.85% 
+Wednesday    103 commits    ███░░░░░░░░░░░░░░░░░░░░░░   14.91% 
+Thursday     94 commits     ███░░░░░░░░░░░░░░░░░░░░░░   13.6% 
+Friday       83 commits     ███░░░░░░░░░░░░░░░░░░░░░░   12.01% 
+Saturday     104 commits    ███░░░░░░░░░░░░░░░░░░░░░░   15.05% 
+Sunday       139 commits    █████░░░░░░░░░░░░░░░░░░░░   20.12%
+
+```
+
+
+📊 **This Week I Spent My Time On** 
+
+```text
+⌚︎ Time Zone: Asia/Kolkata
+
+💬 Programming Languages: 
+JavaScript               13 hrs 22 mins      ███████████████████░░░░░░   77.63% 
+YAML                     1 hr 13 mins        █░░░░░░░░░░░░░░░░░░░░░░░░   7.14% 
+Java                     56 mins             █░░░░░░░░░░░░░░░░░░░░░░░░   5.45% 
+JSON                     46 mins             █░░░░░░░░░░░░░░░░░░░░░░░░   4.55% 
+HTML                     16 mins             ░░░░░░░░░░░░░░░░░░░░░░░░░   1.6%
+
+🔥 Editors: 
+WebStorm                 16 hrs 3 mins       ███████████████████████░░   93.19% 
+Android Studio           1 hr 10 mins        █░░░░░░░░░░░░░░░░░░░░░░░░   6.81%
+
+💻 Operating System: 
+Mac                      17 hrs 13 mins      █████████████████████████   100.0%
+
+```
+
+**I Mostly Code in JavaScript** 
+
+```text
+JavaScript               21 repos            ██████████░░░░░░░░░░░░░░░   41.18% 
+Vue                      8 repos             ████░░░░░░░░░░░░░░░░░░░░░   15.69% 
+Java                     7 repos             ███░░░░░░░░░░░░░░░░░░░░░░   13.73% 
+Dart                     4 repos             ██░░░░░░░░░░░░░░░░░░░░░░░   7.84% 
+CSS                      4 repos             ██░░░░░░░░░░░░░░░░░░░░░░░   7.84%
+
+```
+
+
+
+ Last Updated on 09/07/2022 02:14:27 UTC
+<!--END_SECTION:waka-->
+
+**These Readme stats are generated using github action [awesome-readme-stats](https://github.com/anmol098/waka-readme-stats)**
+
+NOTE: Top languages does not indicate my skill level or anything like that. It is just a metric of which languages have been hosted by me on GitHub based on the usage across repositories. There are others which I haven't put up on GitHub.
+<!--stackedit_data:
+eyJoaXN0b3J5IjpbMTI2NjU1ODI4OCwtMTU1MDQ0NTAwOSwtMT
+YyMTcyNTA5XX0=
+-->
